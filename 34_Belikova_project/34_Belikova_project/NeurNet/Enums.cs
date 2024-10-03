3f6f9cee3f1040da1e83286a959f1a2ae6ad066c
Christina Belikova <119342375+ChristielBel@users.noreply.github.com>
2024-10-03 15:32:31 +0300

    Add files via upload

---
+++ 34_Belikova_project/34_Belikova_project/NeurNet/Enums.cs
@@ -0,0 +1,22 @@
+﻿namespace _34_Belikova_project.NeurNet
+{
+   enum NeuronType // тип нейрона
+    {
+        Hidden, //скрытый слой
+        Output //выходной слой
+    }
+
+    enum NetworkMode//режимы работы сети
+    {
+        Train, //обучение
+        Test, //тест
+        Recogn //распознование
+    }
+
+    enum MemoryMode//режимы работы памяти
+    {
+        GET, //считывание
+        SET, //сохранение 
+        INIT //инициализация
+    }
+}
---
+++ 34_Belikova_project/34_Belikova_project/NeurNet/Neuron.cs
@@ -0,0 +1,40 @@
+﻿using static System.Math;
+
+namespace _34_Belikova_project.NeurNet
+{
+    class Neuron
+    {
+        private NeuronType _type; //тип нейрона
+        private double[] _weights; //его веса
+        private double[] _inputs; //его входы
+        private double _output; //его выходы
+        private double _derivative; //производная функции активации
+        //константы для функции активации
+        private double a = 0.01; //параметр функции активации (лякириллу)
+
+        public double[] Weights { get => _weights; set => _weights = value; }
+
+        public double[] Inputs
+        {
+            get { return _inputs; }
+            set { _inputs = value; }
+        }
+
+        public double Output { get => _output; }
+
+        public double Derivative { get => _derivative; }
+
+        //конструктор
+        public Neuron(double[] inputs, double[] weights, NeuronType type)
+        {
+            _type = type;
+            _weights = weights;
+            _inputs = inputs;
+        }
+
+        public void Activator(double[] i, double[] v)
+        {
+
+        }
+    }
+}
