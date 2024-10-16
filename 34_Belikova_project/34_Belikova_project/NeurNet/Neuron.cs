f97c89c64483c83cb4d9b7242a114f2e41c8eb71
ChristinaBel <kristina04bel@gmail.com>
2024-10-16 19:06:23 +0300

    update

--- 34_Belikova_project/34_Belikova_project/NeurNet/Neuron.cs
+++ 34_Belikova_project/34_Belikova_project/NeurNet/Neuron.cs
@@ -32,9 +32,35 @@ namespace _34_Belikova_project.NeurNet
             _inputs = inputs;
         }
 
-        public void Activator(double[] i, double[] v)
+        public void Activator(double[] i, double[] w)
         {
+            double sum = w[0];
+            for (int m = 0; m < i.Length; m++)
+                sum += i[m] * w[m + 1];
+            switch (_type)
+            {
+                case NeuronType.Hidden:
+                    _output = LogisticAF(sum);
+                    _derivative = LogisticAF_Derivativator(sum);
+                    break;
+                case NeuronType.Output:
+                    _output = Exp(sum);
+                    break;
+            }
 
         }
+
+        private double LogisticAF(double sum)
+        {
+            // Логистическая функция активации
+            return 1.0 / (1.0 + Exp(-sum));
+        }
+
+        private double LogisticAF_Derivativator(double sum)
+        {
+            double logistic = LogisticAF(sum);
+            return logistic * (1.0 - logistic);
+        }
+
     }
 }
